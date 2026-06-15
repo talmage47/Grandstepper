dcde2ca997964fab943168b3e7369b3f5271fe06
Talmage Gaisford <07.cap-inducer@icloud.com>
2026-06-15 17:47:55 -0600

    initial commit

---
+++ grandstepper/grandstepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  grandstepperApp.swift
+//  grandstepper
+//
+//  Created by Talmage Gaisford on 6/15/26.
+//
+
+import SwiftUI
+
+@main
+struct grandstepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
